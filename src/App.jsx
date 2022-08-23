f15bce251b267e3d83c181093223c6187afd07a6
MarcusMahliaire <102937325+MarcusMahliaire@users.noreply.github.com>
2022-08-23 12:31:58 -0700

    changed file name

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { Canvas } from "@react-three/fiber";
 import { Loader } from "@react-three/drei";
 import { Suspense } from "react";
 import Scene from "./environment/scene/Scene";
-import "./app.css";
+import "./apps.css";
 import Header from "./components/header/Header";
-// import OrderScreen from "./components/orderScreen/OrderScreen";
 
 export default function App() {
   return (
@@ -16,7 +15,6 @@ export default function App() {
         </Suspense>
       </Canvas>
       <Loader />
-      {/* <OrderScreen /> */}
     </>
   );
 }
