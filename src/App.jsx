4543e157594f94aa74fae0f9f30233804b87adec
Yassin Abdi <cyanwonder21@gmail.com>
2022-08-24 07:05:07 +0300

    Added backend server
    please clean up

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { Canvas } from "@react-three/fiber";
 import { Loader } from "@react-three/drei";
 import { Suspense } from "react";
 import Scene from "./environment/scene/Scene";
-import "./app.css";
+import "./App.css";
 import Header from "./components/header/Header";
-// import OrderScreen from "./components/orderScreen/OrderScreen";
 
 export default function App() {
   return (
